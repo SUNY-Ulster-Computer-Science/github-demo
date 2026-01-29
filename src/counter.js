45ed508027e842e96b7463d96a45c1dea6e23d19
matthew-pisano <matthewpisano14@gmail.com>
2026-01-29 13:57:43 -0500

    Added Comment

--- src/counter.js
+++ src/counter.js
@@ -1,3 +1,7 @@
+/**
+ * Sets up a counter that the user can click to increment.
+ * @param {Element} element The HTML element to hold the counter.
+ */
 export function setupCounter(element) {
   let counter = 0
   const setCounter = (count) => {
